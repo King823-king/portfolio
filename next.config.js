54b29b4e967099a3e0d078ee7dcbbc63f2407a2f
whoami9600 <whoami9623@gmail.com>
2025-03-24 09:03:21 -0400

    init project

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        loader: "default",
         protocol: "https",
+        path: "https://playful-cendol-105543.netlify.app/",
+        domains: ["images.unsplash.com"],
         hostname: "images.unsplash.com",
         unoptimized: true,
       },
